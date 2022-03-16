797109883e5af9baebd3cc99cc9cedf245e77204
Maegan Wilson <maegan@maeganwilson.com>
2022-03-16 06:55:17 -0500

    Adding this one also

--- ChallengeDay19/ChallengeDay19/ContentView.swift
+++ ChallengeDay19/ChallengeDay19/ContentView.swift
@@ -8,9 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedConversion = "length"
+    @State private var inputUnits: UnitLength = UnitLength.miles
+    @State private var outputUnits: UnitLength = UnitLength.miles
+    @State private var inputAmount: Double = 0.0
+
+    var outputAmount: Double {
+        let input = Measurement(value: inputAmount, unit: inputUnits)
+        return input.converted(to: outputUnits).value
+    }
+
+    let choices: [String: [UnitLength]] = ["length": [UnitLength.meters, UnitLength.kilometers, UnitLength.feet, UnitLength.yards, UnitLength.miles]
+//                                       "temp": ["celsius", "fahrenheit", "kelvin"],
+//                                       "time": ["seconds", "minutes", "hours", "days"],
+//                                       "volume": ["milliliters", "liters", "cups", "pints", "gallons"]
+    ]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            Section {
+                Picker("Input units", selection: $inputUnits) {
+                    ForEach(choices["length"]!, id: \.self) {
+                        Text($0.description)
+                            .tag($0)
+                    }
+                }.pickerStyle(.menu)
+
+                TextField("Input value", value: $inputAmount, format: .number)
+                    .keyboardType(.decimalPad)
+            } header: {
+                Text("Input")
+            }
+
+            Section {
+                Picker("Output units", selection: $outputUnits) {
+                    ForEach(choices["length"]!, id: \.self) {
+                        Text($0)
+                            .tag($0)
+                    }
+                }.pickerStyle(.menu)
+                Text(outputAmount, format: .number)
+            } header: {
+                Text("Output")
+            }
+        }
     }
 }
 
